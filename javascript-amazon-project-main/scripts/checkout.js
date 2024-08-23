8627ce2b9e5c835c230cb005eaef4b130d798ec9
PS01K <prathameshshrikhande03@gmail.com>
2024-08-23 10:50:21 +0530

    Start working on challenge exercise 18.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,22 +2,18 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCartFetch } from "../data/cart.js";
 // import '../data/cart-class.js';
 
 async function loadPage() {  
   try {
     // throw 'error1';
 
-    await loadProductsFetch();
+    Promise.all([
+      await loadProductsFetch(),
+      await loadCartFetch()
+    ]);
   
-    const value = await new Promise ((resolve, reject) => {
-      // throw 'error2';      
-      loadCart(() => {
-        // reject('error3');
-        resolve('value3');
-      });
-    });    
   } catch (error) {
     console.log('Unexpected error. Please try again later.');
   }
