61259776a40fe1c5d2a33842233ef07c2306d1c3
PS01K <prathameshshrikhande03@gmail.com>
2024-08-25 13:54:31 +0530

    Load order page.

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -131,8 +131,6 @@ export function loadProducts (fun) {
       };
       return new Product(productDetails);
     });
-
-    console.log('load product');
     
     fun();
   });
--- javascript-amazon-project-main/scripts/orders.js
+++ javascript-amazon-project-main/scripts/orders.js
@@ -1,21 +1,22 @@
 import { orders } from "../data/orders.js";
 import { formatCurrency } from "./utils/money.js";
-// import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { getProduct, loadProducts } from "../data/products.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 console.log(orders);
+loadProducts(renderOrders);
 
 function renderOrders () {
   let orderHTML = '';
 
   orders.forEach(order => {
-    orderHTML = `
-    <div class="order-container">
-      
+    orderHTML += `
+    <div class="order-container">    
       <div class="order-header">
         <div class="order-header-left-section">
           <div class="order-date">
             <div class="order-header-label">Order Placed:</div>
-            <div>${order.orderTime}</div>
+            <div>${getDate(order.orderTime)}</div>
           </div>
           <div class="order-total">
             <div class="order-header-label">Total:</div>
@@ -30,65 +31,53 @@ function renderOrders () {
       </div>
   
       <div class="order-details-grid">
-        <div class="product-image-container">
-          <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
-        </div>
-  
-        <div class="product-details">
-          <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
-          </div>
-          <div class="product-delivery-date">
-            Arriving on: August 15
-          </div>
-          <div class="product-quantity">
-            Quantity: 1
-          </div>
-          <button class="buy-again-button button-primary">
-            <img class="buy-again-icon" src="images/icons/buy-again.png">
-            <span class="buy-again-message">Buy it again</span>
-          </button>
-        </div>
-  
-        <div class="product-actions">
-          <a href="tracking.html">
-            <button class="track-package-button button-secondary">
-              Track package
-            </button>
-          </a>
-        </div>
-  
-        <div class="product-image-container">
-          <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-        </div>
-  
-        <div class="product-details">
-          <div class="product-name">
-            Adults Plain Cotton T-Shirt - 2 Pack
-          </div>
-          <div class="product-delivery-date">
-            Arriving on: August 19
-          </div>
-          <div class="product-quantity">
-            Quantity: 2
-          </div>
-          <button class="buy-again-button button-primary">
-            <img class="buy-again-icon" src="images/icons/buy-again.png">
-            <span class="buy-again-message">Buy it again</span>
-          </button>
-        </div>
-  
-        <div class="product-actions">
-          <a href="tracking.html?orderId=123&productId=456">
-            <button class="track-package-button button-secondary">
-              Track package
-            </button>
-          </a>
-        </div>
+        ${loadProductsHTMl(order)}
       </div>
     </div>
     `;
   });
 
-  document.querySelector('.js-orders-grid').innerHTML = orderHTML;
+  document.querySelector('.js-orders-grid').innerHTML += orderHTML;
+  
+  function loadProductsHTMl (order) {
+    let productsHTML = '';
+    order.products.forEach(product => {
+      const matchingProduct = getProduct(product.productId);
+  
+      productsHTML += `
+      <div class="product-image-container">
+        <img src="${matchingProduct.image}">
+      </div>
+  
+      <div class="product-details">
+        <div class="product-name">
+          ${matchingProduct.name}
+        </div>
+        <div class="product-delivery-date">
+          Arriving on: ${getDate(product.estimatedDeliveryTime)}
+        </div>
+        <div class="product-quantity">
+          Quantity: ${product.quantity}
+        </div>
+        <button class="buy-again-button button-primary">
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        </button>
+      </div>
+  
+      <div class="product-actions">
+        <a href="tracking.html">
+          <button class="track-package-button button-secondary">
+            Track package
+          </button>
+        </a>
+      </div>
+      `;
+    });
+    return productsHTML;
+  }
+
+  function getDate(date) {
+    return dayjs(date).format('MMMM D')
+  }
 }
