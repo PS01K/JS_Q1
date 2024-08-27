5f899fbcea6d17949fc2212451211d495ead7d59
PS01K <prathameshshrikhande03@gmail.com>
2024-08-27 10:22:28 +0530

    Make 'Track package' button interactive.

--- javascript-amazon-project-main/scripts/orders.js
+++ javascript-amazon-project-main/scripts/orders.js
@@ -83,7 +83,7 @@ function renderOrders () {
       </div>
   
       <div class="product-actions">
-        <a href="tracking.html">
+        <a href="tracking.html?orderId=${order.id}&productId=${product.productId}">
           <button class="track-package-button button-secondary">
             Track package
           </button>
